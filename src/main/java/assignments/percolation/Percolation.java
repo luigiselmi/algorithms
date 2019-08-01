ed19e319e6649aeadd324ae0031e40001179f0f9
Luigi Selmi <luigiselmi@gmail.com>
2019-08-01 18:05:28 +0200

    completed all APIs

--- src/main/java/assignments/percolation/Percolation.java
+++ src/main/java/assignments/percolation/Percolation.java
@@ -2,10 +2,12 @@ package assignments.percolation;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class Percolation {
 	
-	private int closedSites;
+	private int blockedSites;
 	private WeightedQuickUnionUF uf;
 	private int n; 
 	private int [][] id;
@@ -16,7 +18,7 @@ public class Percolation {
     	if (n <= 0)
             throw new IllegalArgumentException("Grid dimensions " + n + " must be a positive number.");
     	this.n = n;
-    	closedSites = n;
+    	blockedSites = n*n;
     	id = new int [n][n];
     	uf = new WeightedQuickUnionUF(n*n);
     }
@@ -25,15 +27,15 @@ public class Percolation {
     public void open(int row, int col) {
     	validate(row, col);
     	if( ! isOpen(row, col) ) {
-    		id[row][col] = 1;
-    		closedSites--;
+    		id[row - 1][col - 1] = 1;
+    		blockedSites--;
     		//connect the site to all open neighbors (left, right, up, down)
     		int p = m2a(row, col);
     		if (row - 1 > 0 && isOpen(row - 1, col)) { 
     			int q = m2a(row - 1, col); //up neighbor
     			uf.union(p, q);
     		}
-    		if (row + 1 < n && isOpen(row + 1, col)) {
+    		if (row + 1 <= n && isOpen(row + 1, col)) {
     			int q = m2a(row + 1, col); // down neighbor
     			uf.union(p, q);
     		}
@@ -41,7 +43,7 @@ public class Percolation {
     			int q = m2a(row, col - 1); // left neighbor
     			uf.union(p, q);
     		}
-    		if (col + 1 < n && isOpen(row, col + 1)) {
+    		if (col + 1 <= n && isOpen(row, col + 1)) {
     			int q = m2a(row, col + 1); // right neighbor
     			uf.union(p, q);
     		}
@@ -51,13 +53,13 @@ public class Percolation {
     // Is the site (row, col) open?
     public boolean isOpen(int row, int col) {
     	validate(row, col);
-    	return id[row][col] == 1;
+    	return id[row - 1][col - 1] == 1;
     }
 
     /* 
      * Is the site (row, col) full?
      * A full site is an open site that can be connected 
-     * to an open site in the top row via a chain of 
+     * to an open site in the top row via a chain of  
      * neighboring (left, right, up, down) open sites.
      */
     public boolean isFull(int row, int col) {
@@ -65,7 +67,7 @@ public class Percolation {
     	validate(row, col);
     	int p = m2a(row, col);
     	for (int i = 0; i < n; i++) {
-    		int q = m2a(0,i);
+    		int q = m2a(1, i + 1);
     		if ( uf.connected(p, q) )
     			isFull = true;
     	}
@@ -74,8 +76,7 @@ public class Percolation {
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-    
-    	return n*n - closedSites;
+    	return n*n - blockedSites;
     }
 
     /* 
@@ -86,15 +87,15 @@ public class Percolation {
     public boolean percolates() {
     	boolean percolates = false;
     	for (int i = 0; i < n; i++) {
-    		if ( isFull(n,i) )
+    		if ( isFull(n,i + 1) )
     			percolates = true;
     	}
     	return percolates;
     }
     
-    // map a matrix element (row, col) to a 1D array index
+    // map a matrix element (row, col) to a 1D array index, e.g. (1.1) -> 0
     private int m2a(int row, int col) {
-    	return n + row + col + 1;
+    	return n * (row - 1) + col - 1;
     }
 
     // validate row and column indexes
@@ -118,15 +119,23 @@ public class Percolation {
     public static void main(String[] args) {
     	int n = StdIn.readInt(); // n-by-n grid
     	int T = StdIn.readInt(); // number of experiments
-    	Percolation model = new Percolation(5);
-    	int tests = 1;
-    	while (! model.percolates() && tests <= T) {
-    		 for (int i = 1; i <= n*n; i++) {
-    			 // open randomly the sites
-    		 }
-    			 
-            
+    	Percolation model = new Percolation(n);
+    	double [] thresholds = new double[T];
+    	for (int t = 0; t < T; t++) {
+	    	while (! model.percolates()) {
+	    	    // choose randomly a site among the blocked ones
+	    		int row = StdRandom.uniform(1, n + 1);
+	    		int col = StdRandom.uniform(1, n + 1);
+	    		if ( ! model.isOpen(row, col) )
+	    			model.open(row, col); // open the site
+	    	}
+    	    // when it percolates computes the threshold
+	    	thresholds[t] = (double) model.numberOfOpenSites() / (n*n) ;
     	}
+    	// Stats
+    	double mean = StdStats.mean(thresholds);
+    	double stddev = StdStats.stddev(thresholds);
+    	System.out.println("mean = " + mean + "\nstddev = " + stddev);
     	
     }
 }
