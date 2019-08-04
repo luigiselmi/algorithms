518826a16e4ff71193d9bf3fba17d2cb2c09651e
Luigi Selmi <luigiselmi@gmail.com>
2019-08-04 15:15:27 +0200

    1st version

--- src/main/java/assignments/queues/Permutation.java
+++ src/main/java/assignments/queues/Permutation.java
@@ -1,10 +1,19 @@
 package assignments.queues;
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		int k = StdIn.readInt(); // n-by-n grid
+		String s;
+    	while (!(s = StdIn.readString()).equals("-")) {
+            rq.enqueue(s);
+        }
+    	
+    	for (int i = 0; i < k; i++)
+    		System.out.println("Random string " + i + ": " + rq.sample());
 	}
 
 }
