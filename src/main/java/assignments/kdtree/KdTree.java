ea09f43105b2c0808c1fc7637f7ede05b021cc96
Luigi Selmi <luigiselmi@gmail.com>
2019-10-04 13:05:53 +0200

    initial version java classes assignement part I, week 5 kd-tree

---
+++ src/main/java/assignments/kdtree/KdTree.java
@@ -0,0 +1,28 @@
+package assignments.kdtree;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+
+  public void insert(Point2D p) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void draw() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Point2D nearest(Point2D query) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Iterable<Point2D> range(RectHV rect) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
