462581c4ce20901afc67de34c3f22191007b10f6
Luigi Selmi <luigiselmi@gmail.com>
2019-10-25 17:48:34 +0200

    init code assignment part 2, week 1

---
+++ src/main/java/assignments/wordnet/WordNetTestClient.java
@@ -0,0 +1,21 @@
+package assignments.wordnet;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class WordNetTestClient {
+
+  public static void main(String[] args) {
+    In in = new In(args[0]);
+    Digraph G = new Digraph(in);
+    SAP sap = new SAP(G);
+    while (!StdIn.isEmpty()) {
+        int v = StdIn.readInt();
+        int w = StdIn.readInt();
+        int length   = sap.length(v, w);
+        int ancestor = sap.ancestor(v, w);
+        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+  }
+}
