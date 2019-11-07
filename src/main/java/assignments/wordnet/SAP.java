7e49af7a13b937f39a6703cdae36127d3a28c183
Luigi Selmi <luigiselmi@gmail.com>
2019-11-07 16:53:38 +0100

    completed constructor

--- src/main/java/assignments/wordnet/SAP.java
+++ src/main/java/assignments/wordnet/SAP.java
@@ -1,5 +1,10 @@
 package assignments.wordnet;
 
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class SAP {
 
   //constructor takes a digraph (not necessarily a DAG)
@@ -29,7 +34,16 @@ public class SAP {
 
   // do unit testing of this class
   public static void main(String[] args) {
-    
+    In in = new In(args[0]);
+    Digraph G = new Digraph(in);
+    SAP sap = new SAP(G);
+    while (!StdIn.isEmpty()) {
+        int v = StdIn.readInt();
+        int w = StdIn.readInt();
+        int length   = sap.length(v, w);
+        int ancestor = sap.ancestor(v, w);
+        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+    }
   }
   
 }
--- src/main/java/assignments/wordnet/WordNet.java
+++ src/main/java/assignments/wordnet/WordNet.java
@@ -1,64 +1,89 @@
 package assignments.wordnet;
 
+import java.io.BufferedInputStream;
+
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
-  private ST<String, Integer> synsetsTable;
-  private Iterable<String> nouns;
+  private ST<String, Queue<Integer>> nounsInvertedIndex;
+  private int synsetNum = 0; // number of synsets
+  private Digraph G;
   
   /*
    * Constructor takes the name of the two input files. 
    * The 1st file contains the synsets. Each synset is made up of three elements
    * the synset id, the synset, and the synset definition, separated by a comma.
-   * The constructor put each synset (key) and synset id (value) in a symbol table.
-   * The synset definition is not used. 
+   * Each synset can contain more than one noun so that each noun can be associated
+   * to one or more synset and synset id. 
+   * The constructor builds an inverted index with synset id as key and nouns as values 
+   * The inverted index is based on a symbol table with key of type integer and values 
+   * in a queue of strings so that more than one noun can be assigned to a synset id.
+   * The synset definition (3rd field) is not used. 
+   * The constructor builds the directed graph using the hypenyms of each synset.
    */
   public WordNet(String synsets, String hypernyms) {
     if (synsets == null || hypernyms == null)
-      throw new IllegalArgumentException("The constructor requeires synsets and hypernyms.");
+      throw new IllegalArgumentException("The constructor requires synsets and hypernyms.");
     
-    synsetsTable = new ST<String, Integer>();
+    nounsInvertedIndex = new ST<String, Queue<Integer>>();
     In synsetsIn = new In(synsets);
     while ( !synsetsIn.isEmpty() ) {
-      String line = synsetsIn.readLine();
-      Integer synsetId = Integer.parseInt(line.split(",")[0]);
-      String synset = line.split(",")[1];
-      synsetsTable.put(synset, synsetId);
+      String [] line = synsetsIn.readLine().split(",");
+      String [] nouns = line[1].split(" ");
+      for (int i = 0 ; i < nouns.length; i++) {
+        if (!nounsInvertedIndex.contains(nouns[i])) nounsInvertedIndex.put(nouns[i], new Queue<Integer>());
+        nounsInvertedIndex.get(nouns[i]).enqueue(synsetNum);
+      }
+      synsetNum++;
     }
     
+    G = new Digraph(synsetNum);
     In hypernymsIn = new In(hypernyms);
+    for (int i = 0 ; i < synsetNum - 1; i++) {
+      String [] vertices = hypernymsIn.readLine().split(",");
+        int v = Integer.parseInt(vertices[0]);
+        for (int j = 1; j < vertices.length ; j++) {
+          int w = Integer.parseInt(vertices[j]);
+          G.addEdge(v, w); 
+        }
+    }
+    
     
   }
 
   // returns all WordNet nouns
   public Iterable<String> nouns() {
-    return synsetsTable.keys();
+    return nounsInvertedIndex.keys();
   }
 
-  // is the word a WordNet noun?
+  // is the word a WordNet noun ?
   public boolean isNoun(String word) {
     if (word == null)
       throw new IllegalArgumentException("A word cannot be null.");
-    return synsetsTable.get(word) != null;
+    return nounsInvertedIndex.get(word) != null;
   }
   
-  // distance between nounA and nounB (defined below)
-  /*
+  // distance between nounA and nounB
   public int distance(String nounA, String nounB) {
-    if (nounA == null || nounB == null)
-      throw new IllegalArgumentException("Both nouns must be provided.");
+    if (! this.isNoun(nounA) || ! this.isNoun(nounB))
+      throw new IllegalArgumentException("Both nouns must be not null and WordNet nouns.");
+    return 0;
   }
-  */
-  // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-  // in a shortest ancestral path (defined below)
+  
   /*
+   *  A synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+   *  in a shortest ancestral path.
+   */
   public String sap(String nounA, String nounB) {
-    if (nounA == null || nounB == null)
-      throw new IllegalArgumentException("Both nouns must be provided.");
+    if (! this.isNoun(nounA) || ! this.isNoun(nounB))
+      throw new IllegalArgumentException("Both nouns must be not null and WordNet nouns.");
+    return null;
   }
-  */
+  
   // do unit testing of this class
   public static void main(String[] args) {
     WordNet wn = new WordNet(args[0], args[1]);
@@ -66,7 +91,11 @@ public class WordNet {
     //for (String noun: wn.nouns())
     //  StdOut.println(noun);
     
-    StdOut.printf("\"yellowlegs\" is a noun ? %s ", wn.isNoun("yellowlegs"));
+    // look for synset ids associated to a noun
+    StdOut.printf("Is \"yellowlegs\" a noun ? %s\n", wn.isNoun("yellowlegs"));
+    for (int synsetId: wn.nounsInvertedIndex.get("zone"))   
+      StdOut.printf("synset id for noun \"zone\" is %d\n", synsetId);
+    
   }
 
 }
