de251c8acfab9224fa1ed1a67a89bc551f74b3df
Luigi Selmi <luigiselmi@gmail.com>
2019-11-07 16:55:43 +0100

    removed unused import

--- src/main/java/assignments/wordnet/WordNet.java
+++ src/main/java/assignments/wordnet/WordNet.java
@@ -1,7 +1,5 @@
 package assignments.wordnet;
 
-import java.io.BufferedInputStream;
-
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
