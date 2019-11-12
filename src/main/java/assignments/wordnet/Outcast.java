5756cb2158510753cc0b61384cc83bfa6faf5875
Luigi Selmi <luigiselmi@gmail.com>
2019-11-12 13:40:38 +0100

    minor change, no implementation

--- src/main/java/assignments/wordnet/Outcast.java
+++ src/main/java/assignments/wordnet/Outcast.java
@@ -12,10 +12,10 @@ public class Outcast {
   
   //given an array of WordNet nouns, return an outcast
   public String outcast(String[] nouns) {
-    
+    return null;
   }
   
-  public static void main(String[] args)  
+  public static void main(String[] args) {  
     WordNet wordnet = new WordNet(args[0], args[1]);
     Outcast outcast = new Outcast(wordnet);
     for (int t = 2; t < args.length; t++) {
@@ -23,4 +23,5 @@ public class Outcast {
       String[] nouns = in.readAllStrings();
       StdOut.println(args[t] + ": " + outcast.outcast(nouns));
     }
+  }
 }
