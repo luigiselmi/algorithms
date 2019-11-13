32c19f4f40f5209ce26e10209a1e95c03ac911de
Luigi Selmi <luigiselmi@gmail.com>
2019-11-13 11:40:08 +0100

    completed shortest ancestral path and synsets' distance

--- src/main/java/assignments/wordnet/WordNet.java
+++ src/main/java/assignments/wordnet/WordNet.java
@@ -1,8 +1,6 @@
 package assignments.wordnet;
 
-import edu.princeton.cs.algs4.DepthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.ST;
@@ -11,17 +9,22 @@ import edu.princeton.cs.algs4.Topological;
 
 public class WordNet {
   private ST<String, Queue<Integer>> nounsInvertedIndex;
+  private ST<Integer, Queue<String>> synsetsIndex;
   private int synsetNum = 0; // number of synsets
   private Digraph G;
-  private int rootVertex;
+  private SAP sap;
+  //private int rootVertex;
   
   /*
    * The constructor takes the name of two input files. 
    * The 1st file contains the synsets. Each synset is made up of three elements
-   * the synset id, the synset, and the synset definition, separated by a comma.
+   * the synset id (integer), the synset (string), and the synset definition, separated by a comma.
    * Each synset can contain more than one noun so that each noun can be associated
    * to one or more synset and synset id. 
-   * The constructor builds an inverted index with synset id as key and nouns as values 
+   * The constructor builds two indices: the synset ids index, and the nouns inverted index.
+   * The index of synset ids is based on a symbol table and uses a synset id as key and a queue of
+   * strings for nouns associated to each synset id.
+   * The inverted index uses noun as key and synset ids as values 
    * The inverted index is based on a symbol table with key of type integer and values 
    * in a queue of strings so that more than one noun can be assigned to a synset id.
    * The synset definition (3rd field) is not used. 
@@ -30,14 +33,18 @@ public class WordNet {
   public WordNet(String synsets, String hypernyms) {
     if (synsets == null || hypernyms == null)
       throw new IllegalArgumentException("The constructor requires synsets and hypernyms.");
-    
+    synsetsIndex = new ST<Integer, Queue<String>>(); 
     nounsInvertedIndex = new ST<String, Queue<Integer>>();
     In synsetsIn = new In(synsets);
     while ( !synsetsIn.isEmpty() ) {
       String [] line = synsetsIn.readLine().split(",");
       String [] nouns = line[1].split(" ");
       for (int i = 0 ; i < nouns.length; i++) {
-        if (!nounsInvertedIndex.contains(nouns[i])) nounsInvertedIndex.put(nouns[i], new Queue<Integer>());
+        // populate the synset ids index
+        if (! synsetsIndex.contains(synsetNum)) synsetsIndex.put(synsetNum, new Queue<String>());
+        synsetsIndex.get(synsetNum).enqueue(nouns[i]);
+        // populate the nouns inverted index
+        if (! nounsInvertedIndex.contains(nouns[i])) nounsInvertedIndex.put(nouns[i], new Queue<Integer>());
         nounsInvertedIndex.get(nouns[i]).enqueue(synsetNum);
       }
       synsetNum++;
@@ -57,6 +64,8 @@ public class WordNet {
     if ( ! isRootedDAG(G) )
       throw new IllegalArgumentException("The input to the constructor does not correspond to a rooted DAG.");
     
+    sap = new SAP(G);
+    
   }
   /*
    * A directed graph (digraph) has a (topological) order 
@@ -71,11 +80,9 @@ public class WordNet {
       for (int v = 0; v < G.V(); v++) 
         if (G.outdegree(v) == 0) {
           numberOfVerticesWithoutOutgoingEdges++;  
-          rootVertex = v;
+          //rootVertex = v;
         }
-    
     boolean isRootedDAG = hasOrder && (numberOfVerticesWithoutOutgoingEdges == 1);
-    
     return isRootedDAG;
   }
 
@@ -96,9 +103,9 @@ public class WordNet {
     if (! this.isNoun(nounA) || ! this.isNoun(nounB))
       throw new IllegalArgumentException("Both nouns must be not null and WordNet nouns.");
     Queue<Integer> synsetsA = nounsInvertedIndex.get(nounA);
-    Queue<Integer> synsetB = nounsInvertedIndex.get(nounB);
-    return 0; // not completed
-          
+    Queue<Integer> synsetsB = nounsInvertedIndex.get(nounB);
+    int distance = sap.length(synsetsA, synsetsB);
+    return distance;
   }
   
   /*
@@ -108,7 +115,10 @@ public class WordNet {
   public String sap(String nounA, String nounB) {
     if (! this.isNoun(nounA) || ! this.isNoun(nounB))
       throw new IllegalArgumentException("Both nouns must be not null and WordNet nouns.");
-    return null;
+    Queue<Integer> synsetsA = nounsInvertedIndex.get(nounA);
+    Queue<Integer> synsetsB = nounsInvertedIndex.get(nounB);
+    int commonAncestor = sap.ancestor(synsetsA, synsetsB);
+    return synsetsIndex.get(commonAncestor).toString();
   }
   
   // do unit testing of this class
