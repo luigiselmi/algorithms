3851dffc0845439b69cd0a64ba5ba3627f3afb9d
Luigi Selmi <luigiselmi@gmail.com>
2019-11-28 17:05:34 +0100

    energy function

--- src/main/java/assignments/seam/SeamCarver.java
+++ src/main/java/assignments/seam/SeamCarver.java
@@ -1,42 +1,78 @@
 package assignments.seam;
 
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SeamCarver {
   
   private Picture picture = null;
   private int H = 0; // height of a picture
   private int W = 0; // width of a picture
+  private double [][] energy = null;
+  
   // create a seam carver object based on the given picture
   public SeamCarver(Picture picture) {
     this.picture = picture;
     H = picture.height();
     W = picture.width();
+    energy = new double[H][W];
+    for (int row = 0; row < H; row++)
+      Arrays.fill(energy[row], -1.0);
   }
 
   // current picture
   public Picture picture() {
-    return picture; // to be implemented
+    return picture;
   }
 
   // width of current picture
   public int width() {
-    return W; // to be implemented
+    return W;
   }
 
   // height of current picture
   public int height() {
-    return H; // to be implemented
+    return H;
   }
 
-  // energy of pixel at column x and row y
+  /*
+   * Computes the energy of pixel at column x and row y
+   */
   public double energy(int x, int y) {
     if (x < 0 || x > W)
       throw new IllegalArgumentException("x is outside its prescribed range.");
     if (y < 0 || y > H)
       throw new IllegalArgumentException("y is outside its prescribed range.");
     
-    return 0.0; // to be implemented
+    if (energy != null && energy[x][y] > 0.0) 
+      return energy[x][y];
+    else 
+      energy[x][y] = Math.sqrt( energyX(x, y) + energyY(x, y) );
+    return energy[x][y];
+ 
+  }
+  
+  /*
+   * Computes the x-component of the gradient energy function
+   */
+  private double energyX(int x, int y) {
+    int deltaRGBx = picture.getRGB(x + 1, y) - picture.getRGB(x - 1, y);
+    int dRx = (deltaRGBx >> 16) & 0xFF;
+    int dGx = (deltaRGBx >>  8) & 0xFF;
+    int dBx = (deltaRGBx >>  0) & 0xFF;
+    return dRx*dRx + dGx*dGx + dBx*dBx;
+  }
+  /*
+   * Computes the y-component of the gradient energy function
+   */
+  private double energyY(int x, int y) {
+    int deltaRGBy = picture.getRGB(x, y + 1) - picture.getRGB(x, y - 1);
+    int dRy = (deltaRGBy >> 16) & 0xFF;
+    int dGy = (deltaRGBy >>  8) & 0xFF;
+    int dBy = (deltaRGBy >>  0) & 0xFF;
+    return dRy*dRy + dGy*dGy + dBy*dBy;
   }
 
   // sequence of indices for horizontal seam
@@ -63,6 +99,12 @@ public class SeamCarver {
 
   //  unit testing (optional)
   public static void main(String[] args) {
-    
+    Picture pic = new Picture(args[0]);
+    SeamCarver seam = new SeamCarver(pic);
+    int width = seam.width();
+    int height = seam.height();
+    for (int x = 1; x < width; x++)
+      for (int y = 1; y < height; y++)
+        StdOut.printf("%f ", seam.energy(x, y));
   }
 }
