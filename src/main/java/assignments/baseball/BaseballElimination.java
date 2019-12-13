f06c3bf46de650a3d9341230f6b21c263a0dd693
Luigi Selmi <luigiselmi@gmail.com>
2019-12-13 21:10:12 +0100

    implemented reading of data

--- src/main/java/assignments/baseball/BaseballElimination.java
+++ src/main/java/assignments/baseball/BaseballElimination.java
@@ -1,42 +1,86 @@
 package assignments.baseball;
 
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
+  
+  private int numberOfTeams = 0;
+  private ST<String, Integer> teams; // names and indexes
+  private int [] w; // wins for each team in the division
+  private int [] l; // losses for each team in the division
+  private int [] r; // remaining games for each team in the division
+  private int [][] g; // games left to play against any other team in the division
+  
   //create a baseball division from given filename in format specified below
   public BaseballElimination(String filename) {
-    // to be implemented
+    if (filename == null) {
+      StdOut.println("Usage:\njava BaseballElimination [data_filename]");
+      return;
+    }
+    
+    In in = new In(filename);
+    numberOfTeams = in.readInt();
+    teams = new ST<String, Integer>();
+    w = new int [numberOfTeams];
+    l = new int [numberOfTeams];
+    r = new int [numberOfTeams];
+    g = new int [numberOfTeams][numberOfTeams];
+    
+    for (int i = 0; i < numberOfTeams; i++) {
+      teams.put(in.readString(), i);
+      w[i] = in.readInt();
+      l[i] = in.readInt();
+      r[i] = in.readInt();
+      for (int j = 0; j < numberOfTeams; j++)
+        g[i][j] = in.readInt();
+    }
   }
   // number of teams
   public int numberOfTeams() {
-    return 0; // to be implemented
+    return numberOfTeams;
   }
   // all teams
   public Iterable<String> teams() {
-    return null; // to be implemented
+    return teams.keys();
   }
   // number of wins for given team
   public int wins(String team) {
-    return 0; // to be implemented
+    if (team == null || ! teams.contains(team))
+      throw new IllegalArgumentException("A team name from the dataset must be specified.");
+    return w[teams.get(team)]; 
   }
   // number of losses for given team
   public int losses(String team) {
-    return 0; // to be implemented
+    if (team == null || ! teams.contains(team))
+      throw new IllegalArgumentException("A team name from the dataset must be specified.");
+    return l[teams.get(team)];
   }
   // number of remaining games for given team
   public int remaining(String team) {
-    return 0; // to be implemented
+    if (team == null || ! teams.contains(team))
+      throw new IllegalArgumentException("A team name from the dataset must be specified.");
+    return r[teams.get(team)]; 
   }
   // number of remaining games between team1 and team2
   public int against(String team1, String team2) {
-    return 0; // to be implemented
+    if (team1 == null || team2 == null || ! teams.contains(team1) || ! teams.contains(team2))
+      throw new IllegalArgumentException("Two team names from the dataset must be specified.");
+    return g[teams.get(team1)][teams.get(team2)];
   }
   // is given team eliminated?
   public boolean isEliminated(String team) {
+    if (team == null || ! teams.contains(team))
+      throw new IllegalArgumentException("A team name from the dataset must be specified.");
     return false; // to be implemented
   }
   // subset R of teams that eliminates given team; null if not eliminated
   public Iterable<String> certificateOfElimination(String team) {
+    if (team == null || ! teams.contains(team))
+      throw new IllegalArgumentException("A team name from the dataset must be specified.");
     return null; // to be implemented
   }
   public static void main(String[] args) {
