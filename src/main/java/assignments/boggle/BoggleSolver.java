36ed7b23eeea6aac407697885aba47b108d2e274
Luigi Selmi <luigiselmi@gmail.com>
2020-01-15 23:10:18 +0100

    api for assignment part 2, week 4

---
+++ src/main/java/assignments/boggle/BoggleSolver.java
@@ -0,0 +1,21 @@
+package assignments.boggle;
+
+public class BoggleSolver {
+  // Initializes the data structure using the given array of strings as the dictionary.
+  // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+  public BoggleSolver(String[] dictionary) {
+    // to be
+  }  
+
+  // Returns the set of all valid words in the given Boggle board, as an Iterable.
+  public Iterable<String> getAllValidWords(BoggleBoard board) {
+    return null; // to be implemented
+  }
+
+  // Returns the score of the given word if it is in the dictionary, zero otherwise.
+  // (You can assume the word contains only the uppercase letters A through Z.)
+  public int scoreOf(String word) {
+    return -1; // to be implemented
+  }
+
+}
