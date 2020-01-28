6f387a98d4511e3aceec2e6a16e74ecae7fd81f6
Luigi Selmi <luigiselmi@gmail.com>
2020-01-28 14:11:47 +0100

    minor change

--- src/main/java/fundamentals/StandardInput.java
+++ src/main/java/fundamentals/StandardInput.java
@@ -1,3 +1,7 @@
+/**
+ * Example usage of StdIn. Write double numbers
+ * and Ctrl+Z to exit the loop 
+ */
 package fundamentals;
 
 import edu.princeton.cs.algs4.StdIn;
@@ -10,7 +14,7 @@ public class StandardInput {
 		double sum = 0.0;
 		int cnt = 0;
 		while (!StdIn.isEmpty())
-		{ // Read a number and cumulate the sum.
+		{ // Read a number and accumulate the sum.
 			sum += StdIn.readDouble();
 			cnt++;
 		}
