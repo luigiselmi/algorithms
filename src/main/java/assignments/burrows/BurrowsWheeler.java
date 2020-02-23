16e43834542e12879db52fbdcc74e6a396c6b590
Luigi Selmi <luigiselmi@gmail.com>
2020-02-23 23:00:11 +0100

    java api for the assignment part 2, week 5

---
+++ src/main/java/assignments/burrows/BurrowsWheeler.java
@@ -0,0 +1,22 @@
+package assignments.burrows;
+
+public class BurrowsWheeler {
+
+  //apply Burrows-Wheeler transform,
+  // reading from standard input and writing to standard output 
+  public static void transform() {
+    
+  }
+
+  // apply Burrows-Wheeler inverse transform,
+  // reading from standard input and writing to standard output
+  public static void inverseTransform() {
+    
+  }
+
+  // if args[0] is "-", apply Burrows-Wheeler transform
+  // if args[0] is "+", apply Burrows-Wheeler inverse transform
+  public static void main(String[] args) {
+    
+  }
+}
