deb4aa135b91f851cb9a38e84dd8130806f79717
Luigi Selmi <luigiselmi@gmail.com>
2020-07-09 00:17:06 +0200

    test

--- src/main/java/assignments/percolation/Percolation.java
+++ src/main/java/assignments/percolation/Percolation.java
@@ -1,16 +1,15 @@
 package assignments.percolation;
 
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 
 public class Percolation {
 	
   private int blockedSites;
-	private WeightedQuickUnionUF uf;
-	private int n; 
-	private int [][] id;
+	private final WeightedQuickUnionUF uf;
+	private final int n;
+	private boolean[][] openSites;
 	
 
   // creates n-by-n grid, with all sites initially blocked
@@ -19,32 +18,41 @@ public class Percolation {
       throw new IllegalArgumentException("Grid dimensions " + n + " must be a positive number.");
     this.n = n;
     blockedSites = n*n;
-    id = new int [n][n];
+    openSites = new boolean [n][n];
     uf = new WeightedQuickUnionUF(n*n);
   }
 
-  // Opens the site (row, col) if it is not open already.
+  /** Opens the site (row, col) if it is not open already.
+   * open() method should do three things: 
+   * 1) it should validate the indices of the site that it receives 
+   * 2) it should somehow mark the site as open
+   * 3) it should perform some sequence of WeightedQuickUnionUF operations 
+   *    that links the site in question to its open neighbors
+   * @param row
+   * @param col
+   */
+  
   public void open(int row, int col) {
     validate(row, col);
-    if( ! isOpen(row, col) ) {
-      id[row - 1][col - 1] = 1;
+    if (!isOpen(row, col)) {
+      openSites[row - 1][col - 1] = true; // set the site to open
     	blockedSites--;
-    	//connect the site to all open neighbors (left, right, up, down)
-    	int p = m2a(row, col);
+    	// connect the site to all open neighbors (left, right, up, down)
+    	int p = xyTo1D(row, col);
     	if (row - 1 > 0 && isOpen(row - 1, col)) { 
-    		int q = m2a(row - 1, col); //up neighbor
+    		int q = xyTo1D(row - 1, col); //up neighbor
     		uf.union(p, q);
     }
     if (row + 1 <= n && isOpen(row + 1, col)) {
-      int q = m2a(row + 1, col); // down neighbor
+      int q = xyTo1D(row + 1, col); // down neighbor
     	uf.union(p, q);
     }
     if (col - 1 > 0 && isOpen(row, col - 1)) {
-      int q = m2a(row, col - 1); // left neighbor
+      int q = xyTo1D(row, col - 1); // left neighbor
     	uf.union(p, q);
     }
     if (col + 1 <= n && isOpen(row, col + 1)) {
-      int q = m2a(row, col + 1); // right neighbor
+      int q = xyTo1D(row, col + 1); // right neighbor
     	uf.union(p, q);
     }
    }
@@ -53,24 +61,28 @@ public class Percolation {
   // Is the site (row, col) open?
   public boolean isOpen(int row, int col) {
     validate(row, col);
-    return id[row - 1][col - 1] == 1;
+    return openSites[row - 1][col - 1];
   }
 
-  /* 
-   * Is the site (row, col) full?
+  /**
    * A full site is an open site that can be connected 
    * to an open site in the top row via a chain of  
    * neighboring (left, right, up, down) open sites.
    */
   public boolean isFull(int row, int col) {
+    validate(row, col);
     boolean isFull = false;
-  	validate(row, col);
-  	int p = m2a(row, col);
-  	for (int i = 0; i < n; i++) {
-  		int q = m2a(1, i + 1);
-  		if ( uf.connected(p, q) )
-  			isFull = true;
-  	}
+    if (isOpen(row, col)) {
+      int p = xyTo1D(row, col);
+      int rootP = uf.find(p);
+      for (int i = 1; i <= n; i++) {
+        if (isOpen(1, i)) {
+          int rootQ = uf.find(i);
+          if (rootP ==  rootQ)
+            isFull = true;
+        }
+      }
+    }
   	return isFull;
   }
 
@@ -87,14 +99,14 @@ public class Percolation {
   public boolean percolates() {
   	boolean percolates = false;
   	for (int i = 0; i < n; i++) {
-  		if ( isFull(n,i + 1) )
+  		if (isFull(n,i + 1) )
   			percolates = true;
   	}
   	return percolates;
   }
   
   // map a matrix element (row, col) to a 1D array index, e.g. (1.1) -> 0
-  private int m2a(int row, int col) {
+  private int xyTo1D(int row, int col) {
   	return n * (row - 1) + col - 1;
   }
 
@@ -108,38 +120,22 @@ public class Percolation {
       }
   }
   
-  /*
-   * 1) Initialize all sites to be blocked.
-   * 2) Repeat the following until the system percolates:
-   *       Choose a site uniformly at random among all blocked sites.
-   *       Open the site.
-   * 3) The fraction of sites that are opened when the system percolates 
-   *    provides an estimate of the percolation threshold.
-   */
   public static void main(String[] args) {
-    int n = StdIn.readInt(); // n-by-n grid
-  	int T = StdIn.readInt(); // number of experiments
-  	Percolation model = new Percolation(n);
-  	double [] thresholds = new double[T];
-  	for (int t = 0; t < T; t++) {
-    	while (! model.percolates()) {
-    	  // choose randomly a site among the blocked ones
-    		int row = StdRandom.uniform(1, n + 1);
-    		int col = StdRandom.uniform(1, n + 1);
-    		if ( ! model.isOpen(row, col) )
-    			model.open(row, col); // open the site
-    	}
-  	  // when it percolates computes the threshold
-    	thresholds[t] = (double) model.numberOfOpenSites() / (n*n) ;
-  	}
+    // int n = StdIn.readInt(); // n-by-n grid
+    int n = 10;
+  	Percolation s = new Percolation(n);
+  	s.open(1, 1);
+  	s.open(1, 2);
+  	int p = s.xyTo1D(1,1);
+  	int q = s.xyTo1D(1,2);
+  	boolean test1 = s.uf.find(p) == s.uf.find(q);
+  	StdOut.printf("%s\n", test1);
   	
-  	// Stats
-  	double mean = StdStats.mean(thresholds);
-  	double stddev = StdStats.stddev(thresholds);
-  	double confidenceLo = mean - 1.96 * stddev / Math.sqrt(T);
-  	double confidenceHi = mean + 1.96 * stddev / Math.sqrt(T);
-  	System.out.println("mean = " + mean + "\nstddev = " + stddev);
-  	System.out.println("95% confidence interval = [" + confidenceLo + ", " + confidenceHi + "]");
-  	
+  	In in = new In(args[0]);
+  	n = in.readInt();
+  	s = new Percolation(n);
+  	s.open(1, 6);
+  	boolean test2 = s.isOpen(1, 6) == s.isFull(1, 6);
+  	StdOut.printf("%s\n", test2);
   }
 }
--- src/main/java/assignments/percolation/PercolationStats.java
+++ src/main/java/assignments/percolation/PercolationStats.java
@@ -1,67 +1,78 @@
 package assignments.percolation;
 
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 	
-	private Percolation model;
-	private int n;
+	private final int n;
 	private double [] thresholds;
+	private final int trials;
 	
 	// perform independent trials on an n-by-n grid
 	public PercolationStats(int n, int trials) {
-		this.n = n;
-		if (n <= 0 || trials <= 0)
+	  if (n <= 0 || trials <= 0)
             throw new IllegalArgumentException("Grid dimensions " + n + " must be a positive number and at least one trial " + trials + " should be performed");
-    	
-    	model = new Percolation(n);
-    	thresholds = new double[trials];
-    	executeTrials();
-    }
+	  
+	  this.n = n;
+	  this.trials = trials;
+    thresholds = new double[trials];
+    executeSetOfTrials(trials);
+  }
 
-    // sample mean of percolation threshold
-    public double mean() {
-    	return StdStats.mean(thresholds);
-    }
+  // sample mean of percolation threshold
+  public double mean() {
+  	return StdStats.mean(thresholds);
+  }
 
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-    	return StdStats.stddev(thresholds);
-    }
+  // sample standard deviation of percolation threshold
+  public double stddev() {
+    if (trials == 1)
+      return Double.NaN;
+    else
+      return StdStats.stddev(thresholds);
+  }
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-    	return mean() - 1.96 * stddev() / Math.sqrt(thresholds.length);
-    }
+  // low endpoint of 95% confidence interval
+  public double confidenceLo() {
+  	return mean() - 1.96 * stddev() / Math.sqrt(trials);
+  }
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-    	return mean() + 1.96 * stddev() / Math.sqrt(thresholds.length);
+  // high endpoint of 95% confidence interval
+  public double confidenceHi() {
+  	return mean() + 1.96 * stddev() / Math.sqrt(trials);
+  }
+  
+  /**
+   * 1) Choose randomly a site among the blocked ones
+   * 2) Open the site
+   * @param t
+   */
+  private void executeTrial(int t) {
+    Percolation system = new Percolation(n);
+    while (! system.percolates()) {
+      int row = StdRandom.uniform(1, n + 1);
+      int col = StdRandom.uniform(1, n + 1);
+      if ( ! system.isOpen(row, col) )
+        system.open(row, col); // open the site
     }
+    thresholds[t] = (double) system.numberOfOpenSites() / (n*n) ; // when it percolates computes the threshold
+  }
 
-    private void executeTrials() {
-    	for (int t = 0; t < thresholds.length; t++) {
-	    	while (! model.percolates()) {
-	    	    // choose randomly a site among the blocked ones
-	    		int row = StdRandom.uniform(1, n + 1);
-	    		int col = StdRandom.uniform(1, n + 1);
-	    		if ( ! model.isOpen(row, col) )
-	    			model.open(row, col); // open the site
-	    	}
-    	    // when it percolates computes the threshold
-	    	thresholds[t] = (double) model.numberOfOpenSites() / (n*n) ;
-    	}
-    }
-    // test client (see below)
-    public static void main(String[] args) {
-    	int n = StdIn.readInt(); // n-by-n grid
-    	int trials = StdIn.readInt(); // number of experiments
-    	PercolationStats stats = new PercolationStats(n, trials);
-    	System.out.println("mean = " + stats.mean());
-    	System.out.println("stddev = " + stats.stddev());
-    	System.out.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
-    }
+  private void executeSetOfTrials(int trials) {
+  	for (int t = 0; t < trials; t++) 
+  	  executeTrial(t);
+  }
+  
+  public static void main(String[] args) {
+  	int n = StdIn.readInt(); // builds n-by-n grid
+  	int trials = StdIn.readInt(); // number of experiments
+  	PercolationStats stats = new PercolationStats(n, trials);
+  	StdOut.printf("Mean = %f\n", stats.mean());
+  	StdOut.printf("Std. dev. = %f\n", stats.stddev());
+  	StdOut.printf("95 Pct. confidence interval = [%f, %f]\n", stats.confidenceLo(), stats.confidenceHi());
+  }
     
 }
