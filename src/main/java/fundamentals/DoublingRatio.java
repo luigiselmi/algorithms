2864b35ee3b6a0b473e936016c3c4982e7b86588
Luigi Selmi <luigiselmi@gmail.com>
2020-07-27 11:41:53 +0200

    removed unused imports

--- src/main/java/fundamentals/DoublingRatio.java
+++ src/main/java/fundamentals/DoublingRatio.java
@@ -1,13 +1,7 @@
 package fundamentals;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Random;
-
 import edu.princeton.cs.algs4.StdOut;
-
 
 public class DoublingRatio {
 	
@@ -32,7 +26,7 @@ public class DoublingRatio {
 		double prev = timeTrial(100);
         for (int n = 200; true; n += n) {
             double time = timeTrial(n);
-            System.out.println("N = " +  n + ", Time = " + time + ", Ratio = " + time/prev);
+            StdOut.printf("N = %d, Time = %f, Ratio = %f",  n, time, time/prev);
             prev = time;
         } 
 	}
