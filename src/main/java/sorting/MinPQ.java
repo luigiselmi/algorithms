ed061788307ebe4503540a6fdbadf1dfbf1240d1
Luigi Selmi <luigiselmi@gmail.com>
2021-05-24 13:22:46 +0200

    priority queue for min values

---
+++ src/main/java/sorting/MinPQ.java
@@ -0,0 +1,134 @@
+/**
+ * A priority queue based on a binary heap-ordered array.
+ * This priority queue implements an insert() method and 
+ * a delMin() method that returns the minimum value of the 
+ * heap. It is implemented from MaxPQ by simply inverting 
+ * the result of the keys comparison in the less() method.
+ *
+ * @param <Key>
+ */
+package sorting;
+
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class MinPQ<Key extends Comparable<Key>> {
+  
+  private Key[] pq; // heap-ordered complete binary tree
+  private int N = 0;
+  
+  public MinPQ(int m) {
+    pq = (Key[]) new Comparable[m + 1];
+  }
+  
+  /**
+   * Initializes an empty priority queue.
+   */
+  public MinPQ() {
+      this(1);
+  }
+  
+  /**
+   * We add the new key at the end of the array, increment the size of the heap,
+   * and then swim up through the heap with that key to restore the heap condition
+   */
+  public void insert(Key v) {
+    // double size of array if necessary
+    if (N == pq.length - 1) resize(2 * pq.length);
+    pq[++N] = v;
+    swim(N);
+  }
+  
+  /**
+   * We take the smallest key off the top, put the item from
+   * the end of the heap at the top, decrement the size of the heap, and then sink down
+   * through the heap with that key to restore the heap condition
+   */
+  public Key delMin() {
+    Key max = pq[1]; // retrieve min key from top.
+    exch(1, N--); // exchange with last item.
+    pq[N+1] = null; // avoid loitering.
+    sink(1); // restore heap property.
+    if (N > 0 && N == pq.length/4) resize(pq.length/2); // halve size of the array when it's one-quarter full
+    return max;
+  }
+  
+  public Key min() {
+    return pq[1];
+  }
+  
+  public boolean isEmpty() {
+    return N == 0;
+  }
+  
+  public int size() {
+    if (isEmpty()) throw new NoSuchElementException("The heap is empty.");
+    return N;
+  }
+  
+  /**
+   * Restore the heap order when a child's key's node
+   * is bigger that its parent's key. In a heap, the parent of the
+   * node in position k is in position k/2.
+   * @param k
+   */
+  private void swim(int k) {
+    while (k > 1 && less(k/2, k)) {
+      exch(k/2, k);
+      k = k/2;
+    }
+  }
+  
+  /**
+   * Restore heap order when a node's key
+   * is smaller than the one's of its children.
+   * The two children of the node in position
+   * k are in positions 2k and 2k + 1.
+   * @param k
+   */
+  private void sink(int k) {
+    while (2*k <= N)  {
+      int j = 2*k;
+      if (j < N && less(j, j+1)) j++;
+      if (!less(k, j)) break;
+      exch(k, j);
+      k = j;
+    }
+  }
+   
+  private boolean less(int i, int j) { 
+    return pq[i].compareTo(pq[j]) > 0; 
+  }
+  
+  private void exch(int i, int j) { 
+    Key t = pq[i]; pq[i] = pq[j]; pq[j] = t; 
+  }
+  
+  // double the size of the heap array
+  private void resize(int capacity) {
+      Key[] temp = (Key[]) new Comparable[capacity];
+      for (int i = 1; i <= N; i++) {
+          temp[i] = pq[i];
+      }
+      pq = temp;
+  }
+  /*
+   * To test the priority queue, write in the command line the number of operations
+   * i.e. additions or removals of keys, you are going to perform and then the keys 
+   * (e.g. characters), one by one. You can remove the root entering a "-" character. 
+   */
+  public static void main(String[] args) {
+    int numOperations = 0;
+    int maxOperations = StdIn.readInt(); // insertion or deletion
+    MinPQ<String> pq = new MinPQ<String>();
+    while (!StdIn.isEmpty() && numOperations < maxOperations) {
+      numOperations++;
+      String item = StdIn.readString();
+      if (!item.equals("-")) pq.insert(item); // insert
+      else if (!pq.isEmpty()) StdOut.print(pq.delMin() + " "); // remove min
+    }
+    StdOut.println("(" + pq.size() + " keys left on the queue)");
+  }
+}
